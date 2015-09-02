026a7667cba1fd93d93247d7201454f0149abfb6
loveandhappiness <georg@geladaris.de>
2015-09-02 08:58:35 +0200

    Replace buttons with Glyphicons

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@ var gulp  = require('gulp');
 var sass  = require('gulp-sass');
 var watch = require('gulp-watch');
 
+gulp.task('copy', function () {
+    gulp.src([
+        'bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.ttf',
+        'bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff',
+        'bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff2'
+    ])
+        .pipe(gulp.dest('fonts/bootstrap'));
+});
+
 gulp.task('sass', function () {
     return gulp.src([
         'sass/app.scss',
